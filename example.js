0ad48d491319d059a41c0c0ac7f6e926ef032bf3
rohitapfmti <96857213+rohitapfmti@users.noreply.github.com>
2022-01-01 13:19:21 +0530

    Add Secondary text and style to popup window

--- example.js
+++ example.js
@@ -3,57 +3,71 @@ import fmtiModalWindow from 'fmti-modal-window';
 //Method If Button clicked
 const onDeletion = ()=>{
    setTimeout(()=>{
-      alert("Test API");
       alert("You clicked me!!");
    },3000);
-}
-
-//On CheckBox Checked
-const onCheckboxHandler = ()=>{
-   fmtiModalWindow.enableButton("FMTI-MODAL-BTN-RIGHT");
-}
-//Checkbox Unchecked Handler
-const onUncheckHandler = ()=>{
-   fmtiModalWindow.disableButton("FMTI-MODAL-BTN-RIGHT");
 }
 
 const data = ()=>{
     document.getElementById("domRl").addEventListener("click",()=>{
       //Initialization Of Modal Window
-      const modalwindow =  fmtiModalWindow.init({
-      modalWindowId: "apModal",
-      headerInfo:{
-                  showTitle: true,
-                  text: "Purpose Of Our Life"
-                },
-      bodyContentInfo:{
-                text: "The purpose of life is to contribute in some way to making things better",
-                actions: [{ // optional: It displays checkbox in the body just above footer
-                          type: "CHECKBOX", 
-                          text: "Yes, Its True", //Any Random Text
-                          id: "lifeQuote", //Checkbox input element id
-                          value: true, // Check.Uncheck value is true/false
-                          onCheck: onCheckboxHandler,// function Reference
-                          onUnCheck: onUncheckHandler//function Reference
-                      }]
-      },
-       footerInfo:{
-                    btnLeft: {
-                      "show": true,
-                      "enable": true,
-                      "text": "Cancel",
-                      "onclick": "DESTROY"
-                    },
-                    btnRight: {
-                      "show": true,
-                      "enable": false,
-                      "text": "Delete",
-                      "onclick": onDeletion
-                    },
-           }
-
-         });
-         //Promise Subscription 
+     const modalwindow =  fmtiModalWindow.init({
+    containerStyle:{},
+    headerInfo:{
+       showTitle: true,
+       text: "The Purpose of our lives",
+       style:{
+           color: "grey" //Styles can be given like this
+       }
+      
+    },
+    bodyContentInfo:{
+       text: "Everyone thinks of changing the world, but no one thinks of changing himself",
+       style:{
+           color: "black" //Styles can be given like this
+       },
+       secondary:[{ // Added in latest version 
+          text: "Life is either a great adventure or nothing. `Helen Keller",
+          style:{
+             "text-align": "center",
+             "color": "brown"
+          }
+        
+       }],
+       actions:[
+          {
+             type: "checkbox",
+             id: "LittleStart",
+             targetElId: "FMTI-MODAL-BTN-RIGHT", //Button Id enable/disable
+             text: "Hey! Don't show me again",
+             style:{},
+             onCheck: (btnId)=>{
+                fmtiModalWindow.enableButton(btnId);
+             },
+             onUnCheck: (btnId)=>{
+                fmtiModalWindow.disableButton(btnId);
+             }
+          }
+       ]
+    },
+    footerInfo:{
+       btnLeft:{
+          show: true,
+          enable: true,
+          text: "Cancel",
+          onclick:"DESTROY"
+          style:{}
+        
+       },
+       btnRight:{
+          show: true,
+          text: "Delete",
+          enable: false,
+          onclick:onDeletion
+          style:{}
+       }
+    }
+ });
+  //Promise Subscription 
          modalwindow.then((data)=>{
             if(data.success){
                //Open Modal Window
